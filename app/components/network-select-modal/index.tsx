2fc1dac3d9c1412a54aa914374fbcca7c01203c9
helios492 <aphrodite98313@gmail.com>
2024-11-26 20:47:38 +0900

    fix:modify network order and make only westend network active

--- app/components/network-select-modal/index.tsx
+++ app/components/network-select-modal/index.tsx
@@ -6,11 +6,14 @@ import { NetworkKeys } from "@/app/types/enum";
 import Image from "next/image";
 import { useState, useEffect } from "react";
 
-const networks = [
-  { name: NetworkKeys.Rococo },
-  { name: NetworkKeys.Westend },
-  { name: NetworkKeys.Kusama },
-  { name: NetworkKeys.Polkadot },
+const liveNetworks = [
+  { name: NetworkKeys.Polkadot, active:false },
+  { name: NetworkKeys.Kusama, active:false },
+]
+
+const testNetworks = [
+  { name: NetworkKeys.Westend, active:true },
+  { name: NetworkKeys.Rococo, active:false },
 ]
 
 const NetworkSelectModal = () => {
@@ -32,28 +35,61 @@ const NetworkSelectModal = () => {
 
   return (
     <Modal isOpen={networkModal} onClose={() => setNetworkModal(false)} title="Select Network">
-      <div className="grid grid-cols-2 gap-4">
-      {networks.map((item, idx) => (
-        <button
-          key={idx}
-          className={classNames(
-            "flex flex-row justify-between items-center w-full hover:bg-[#5000fe4f] dark:hover:bg-[white] p-2 rounded-lg",
-            { "bg-[#5000fe4f] dark:bg-[white]": item.name === network }
-          )}
-          onClick={() => switchNetwork(item.name)}
-        >
-        <div className="flex flex-row justify-center items-center gap-3">
-            <div className="h-full rounded-full bg-gradient-to-r from-[#5100FE] to-[#32009C] p-1">
-            </div>
-            <Image src={`/network-icons/${item.name}.svg`} alt={item.name} width={32} height={32} />
-            <div className="flex flex-col text-left text-[#b4d2ffaf] dark:text-[#120038]">
-              <p className="text-lg font-bold">
-              {item.name}
-            </p>
+      <div className="grid grid-cols-1 gap-4">
+        <div>
+          <p>Live Network</p>
+          <div className="grid grid-cols-2 gap-5">
+          {liveNetworks.map((item, idx) => (
+              <button
+                key={idx}
+                className={classNames(
+                  `flex flex-row justify-between items-center w-full {item.active && hover:bg-[#5000fe4f]} {item.active && dark:hover:bg-[white]} p-2 rounded-lg`,
+                  { "bg-[#5000fe4f] dark:bg-[white]": item.name === network }
+                )}
+                disabled = {!item.active}
+                onClick={() => switchNetwork(item.name)}
+              >
+                <div className="flex flex-row justify-center items-center gap-3">
+                  <div className="h-full rounded-full bg-gradient-to-r from-[#5100FE] to-[#32009C] p-1">
+                  </div>
+                  <Image src={`/network-icons/${item.name}.svg`} alt={item.name} width={32} height={32} />
+                  <div className="flex flex-col text-left text-[#b4d2ffaf] dark:text-[#120038]">
+                    <p className="text-lg font-bold">
+                      {item.name}
+                    </p>
+                  </div>
+                </div>
+              </button>
+          ))}
           </div>
+        </div>
+        <div>
+          <p>Test Network</p>
+          <div className="grid grid-cols-2 gap-5">
+          {testNetworks.map((item, idx) => (
+            <button
+              key={idx}
+              className={classNames(
+                "flex flex-row justify-between items-center w-full {item.active && hover:bg-[#5000fe4f]} {item.active && dark:hover:bg-[white]} p-2 rounded-lg",
+                { "bg-[#5000fe4f] dark:bg-[white]": item.name === network }
+              )}
+              disabled = {!item.active}
+              onClick={() => switchNetwork(item.name)}
+            >
+              <div className="flex flex-row justify-center items-center gap-3">
+                <div className="h-full rounded-full bg-gradient-to-r from-[#5100FE] to-[#32009C] p-1">
+                </div>
+                <Image src={`/network-icons/${item.name}.svg`} alt={item.name} width={32} height={32} />
+                <div className="flex flex-col text-left text-[#b4d2ffaf] dark:text-[#120038]">
+                  <p className="text-lg font-bold">
+                    {item.name}
+                  </p>
+                </div>
+              </div>
+            </button>
+          ))}
           </div>
-        </button>
-      ))}
+        </div>
       </div>
     </Modal>
   );
